d3c60b6ccf43f98435c9633202aceab18f4310a5
Alejandro <alejandro.itslp.isc@gmail.com>
2021-06-30 16:55:49 -0500

    aun funciona

--- app/Http/Livewire/TablaAprov.php
+++ app/Http/Livewire/TablaAprov.php
@@ -5,17 +5,20 @@ namespace App\Http\Livewire;
 use App\Models\Compra;
 use App\Models\Productoscompra;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TablaAprov extends Component
 {
-    public $aproData;
+    use WithPagination;
+
+    
 
     public function render()
     {
 
-        $this->aproData=Compra::where('autorizado', 1)->orderBy('foliocompra','asc')->get();
+        $aproData=Compra::where('autorizado', 1)->orderBy('foliocompra','asc')->get();
         $this->aproProd=Productoscompra::get();
-        return view('livewire.tabla-aprov');
+        return view('livewire.tabla-aprov',compact('aproData'));
     }
 
 }
